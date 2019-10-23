80d9f63491d5166aebbe8a9a3736ffaa549c808d
blakeblake <bdziatkowicz@gmail.com>
2019-10-23 15:25:59 -0400

    revise/to just landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,37 +15,6 @@ const IndexPage = ({ className }) => {
   return (
     <div>
       <HomeSection />
-      <InfoSection
-        theme={light}
-        title="A place unlike anywhere you've built before."
-        subTitle="Augury Square is a tokenized, mixed-use real estate development built on 30 acres of shared workspace, offices, retail, parks, and residential providing state-of-the-art  technologies including Blockchain, IoT, and AI for the community of entrepeneurs, developers, enthusiasts and users to apply for building in a seamlessly integrated lifestyle."
-      />
-      <SplitSection1
-        theme={light}
-        title="The Environment for Innovators"
-        subTitle="The Augury Square is a community where innovators collaborate and connect solutions that matter to the masses. A next generation business hub and incubator that accelerates ideas into existence. Augury Square automates all of the day to day tasks and todos allowing you focus on what maters most."
-      />
-      <TunnelPic />
-      <SplitSection2
-        theme={light}
-        title="Collaborative at the Core"
-        subTitle="Environments are only as amazing as the individuals inside them. Individuals of Augury Square encompass a wide array of unique qualities. They understand that great products start with people more than just top tier professionals, a diverse assortment of innovative minds enabling amazing ideas to impact everyone."
-      />
-      <BannerSection
-        theme={dark}
-        title="Mission:"
-        subTitle="Unleash full potential of people through an interactive approach to living, working and technology."
-      />
-      <SplitSection3
-        theme={light}
-        title="Human-centered innovation relevant to your lifestyle"
-        subTitle="Innovation isn’t only for enterprise, it’s for people too. A human-centered approach to lifestyle makes for a truly unforgettable living experienc, making everyday amazing for the people you love. Augury Square accelerates you to achieve your goals, forming a launchpad for your creativity."
-      />
-      <BannerSection
-        theme={dark}
-        title="About"
-        subTitle="Augury Square is about collaborating through a culture complimentary to your skillsets, interest and expertise connected in a community of doers."
-      />
     </div >
   )
 }

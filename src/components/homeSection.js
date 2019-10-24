52906d85f4d2bd19a994df35f93c2e6d1c97de18
Julian Jacobs <jljacobs45@gmail.com>
2019-10-23 20:09:06 -0700

    including split sections wtih theme context

--- src/components/homeSection.js
+++ src/components/homeSection.js
@@ -8,6 +8,7 @@ import BackgroundImage from 'gatsby-background-image'
 
 import styled, { ThemeContext } from 'styled-components'
 import HomeSummaryBox from "./homeSummaryBox"
+import SplitSection1 from "./splitSection1";
 
 const Div = styled.div`
 @media (min-width: 600px){
@@ -47,6 +48,13 @@ const Home = ({ className }) => {
               <Layout>
                 <SEO title="Augury Square" />
                 <HomeSummaryBox />
+
+                {/* Could probably put the rest of the app here but keeping it in layouts for now */}
+                <SplitSection1 
+                  theme={themeContext}
+                  title="The environment for innovators"
+                  subTitle="The Augury Square is a community where innovators collaborate and connect solutions that matter to the masses. A next generation business hub and incubator that accelerates ideas into existance. Augury Square automates all of the day to day tasks and todos allowing you focus on what maters most."
+                />
 
               </Layout>
             </BackgroundImage>

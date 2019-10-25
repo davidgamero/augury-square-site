9458bf951bf095a863f415586cc51e7549f21648
David Gamero <david340804@gmail.com>
2019-10-25 16:21:51 -0400

    statement auto height on mobile to fix clipping

--- src/components/StatementSection.js
+++ src/components/StatementSection.js
@@ -3,19 +3,29 @@ import styled from 'styled-components'
 
 const Div = styled.div`
   background-color: black;
-  height: 50vh;
+  @media (max-width: 425px) {
+    font-size: 1.3rem;
+    line-height: 2rem;
+    height: auto;
+    padding: 10vh 6vh;
+  }
+  @media (min-wdth: 425px) {
+    height: 50vh;
+    padding: 0 10%;
+  }
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding: 0 10%;
 `
 const H1 = styled.h1`
   font-size: 64px;
   color: white;
+  font-family: Domine;
 `
 const P = styled.p`
   font-size: 24px;
   color: white;
+  font-family: Roboto;
   
   @media (max-width: 425px) {
     font-size: 1.3rem;
